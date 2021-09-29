d928035705412f1d70f3a928479303c4f36d1b3b
Takahashihiroak <89174229+Takahashihiroak@users.noreply.github.com>
2021-09-29 13:20:25 +0900

    修正2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "ユーザー新規登録" do
-    it "nicknameが空だと登録できない" do
-      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
-      user.valid?
-      expect(user.errors.full_messages).to include("Nickname can't be blank")
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it "nicknameとemail、passwordとpassword_confirmation、お名前(全角)とお名前カナ(全角)、生年月日が存在すれば登録できる" do
+        expect(@user).to be_valid
+      end
     end
-    it "emailが空では登録できない" do
-      user = User.new(nickname: "abe", email: "", password: "00000000", password_confirmation: "00000000")
-      user.valid?
-      expect(user.errors.full_messages).to include("Email can't be blank")
+    context '新規登録できない場合' do
+      it "nicknameが空では登録できない" do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it "emailが空では登録できない" do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it "passwordが空では登録できない" do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it "お名前(全角)が空では登録できない" do
+        @user.family_name = ''
+        @user.first_name = ''
+        @user.valid?
+      end
+      it "お名前カナ(全角)が空では登録できない" do
+        @user.family_name_kana = ''
+        @user.first_name_kana = ''
+        @user.valid?
+      end
+      it "生年月日が空では登録できない" do
+        @user.birthday = ''
+        @user.valid?
+      end
     end
   end
 end
