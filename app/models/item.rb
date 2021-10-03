966c4e451d1102d762fb78558cd36c4f9621ddec
Takahashihiroak <89174229+Takahashihiroak@users.noreply.github.com>
2021-10-03 14:20:57 +0900

    修正1

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  belongs_to :user 
+  belongs_to :user
   # has_one :order
   has_one_attached :image
 
@@ -17,15 +17,14 @@ class Item < ApplicationRecord
   validates :prefecture, presence: true
 
   with_options presence: true do
-  validates :image
-  validates :name 
-  validates :text
-  validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+    validates :image
+    validates :name
+    validates :text
+    validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
   end
 
   validates :price, numericality: true
 
-
-  validates :category_id, :condition_id, :delivery_fee_id, :delivery_day_id, :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
-
+  validates :category_id, :condition_id, :delivery_fee_id, :delivery_day_id, :prefecture_id,
+            numericality: { other_than: 1, message: "can't be blank" }
 end
